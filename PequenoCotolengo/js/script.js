18d439fc5bf1fdb850edbb7d5c06f6cdb90b3f30
JoseRodrigues-neto <joserodrigues15ne@gmail.com>
2023-06-13 23:02:32 -0300

    validaçôes

--- PequenoCotolengo/js/script.js
+++ PequenoCotolengo/js/script.js
@@ -42,26 +42,76 @@ function validarCheckbox(event) {
     }
   }
 
-  if (!algumCheckboxMarcado) {
-    event.preventDefault();  
-    alert("Por favor, marque uma area de atuação");
+ const name = document.getElementById("name");
+  const address = document.getElementById("address");
+  const phone = document.getElementById("phone-number");
+  const ig = document.getElementById("social-ig");
+  const email = document.getElementById("social-email");
+  const birthday = document.getElementById("birthday-date");
  
-  } else if (!algumCheckboxMarcado1) {
-    alert("Por favor, marque um hotario");
-  }
-  
-   const name = document.getElementById("name");
   switch (true) {
     case name.value === "" || !validarNome(name.value):
-      alert("Por favor, escreva um nome.");
+      alert("Por favor, escreva um nome válido");
       break;
 
+    case  address.value === "" || !validarEndereco(address.value):
+      alert("Por favor, escreva um endereço");
+      break;
+
+    case  phone.value === "" || !validarTelefone(phone.value):
+      alert("Por favor, insira um telefone válido");
+      break;
+
+    case  ig.value === "" || !validarInstagram(ig.value):
+      alert("Por favor, insira um nome de usuário válido");
+      break;
+
+    case  email.value === "" || !validarEmail(email.value):
+      alert("Por favor, insira um E-mail  válido");
+      break;
+
+   
+    case  birthday.value === "" || !validarAniversario(birthday.value):
+      alert("Por favor, insira um ano válido");
+      break;
+
+    case  !algumCheckboxMarcado1:
+       alert("Por favor, marque um horario");
+       break;
+
+     case  !algumCheckboxMarcado:
+       alert("Por favor, marque uma área de atuação");
+       break;
+      
+      default:
+        alert("enviado com sucesso");
   }
+
+
 function validarNome(name){
-  return /^[A-Za-z\s]+$/.test(name);
+  return  /^[\p{L}\s']+$/u.test(name);
 }
+function validarEndereco(address) {
+    return /^[a-zA-Z0-9\s.,-]+$/.test(address);
+  }
 
+  function validarTelefone(phone) {
+    return /^([14689][0-9]|2[12478]|3([1-5]|[7-8])|5([13-5])|7[193-7])9[0-9]{8}$/.test(phone);
+  }
+  
+  function validarInstagram(ig){
+    return /(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)/.test(ig);
+  }
 
+  function validarEmail (email){
+    return /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email);
+  }
+
+  function validarAniversario (birthday) {
+    return /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/.test(birthday);
+  }
+
+ 
 }
 
 
